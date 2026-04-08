83b936500f36153d149fed557e71993de0c9879e
halaemadali <halaemad177@gmail.com>
2026-04-08 16:45:15 +0200

    Update RoomType.java

--- oopProject/src/RoomType.java
+++ oopProject/src/RoomType.java
@@ -1,34 +1,35 @@
 public class RoomType {
-private String NO3;    //single,doublr,suite
-private int capacity;
-private double basePrice;
+    private String NO3;    //single,double,suite
+    private int capacity;
+    private double basePrice;
 
-  public Room (String NO3 ;int capacity; double basePrice ){
-    this. NO3= NO3;
-    this.capacity=capacity;
-    this.basePrice= basePrice;
-    
-  }
- 
+    public RoomType (String NO3 ,int capacity, double basePrice ){
+        this. NO3= NO3;
+        this.capacity=capacity;
+        this.basePrice= basePrice;
+
+    }
 
 
-public String getNO3(){
-  return  NO3;
-}
-public void setNO3(String n){
-   NO3=n;
-}
-  public int getCapacity(){
-  return capacity ;
-}
-  public void  setCapacity(int c){
-  capacity=c;
-}
-  public double getBasePrice(){
-  return  basePrice;
-}
-  public void setBasePrice(double p){
-    basePrice=p;
-  }
-  
+
+    public String getNO3(){
+        return  NO3;
+    }
+    public void setNO3(String n){
+        NO3=n;
+    }
+    public int getCapacity(){
+        return capacity ;
+    }
+    public void  setCapacity(int c){
+        capacity=c;
+
+    }
+    public double getBasePrice(){
+        return  basePrice;
+    }
+    public void setBasePrice(double p){
+        basePrice=p;
+    }
+
 }
