3308d9c9fe322be9cd0d93fcaa88756e5376bcb7
halaemadali <halaemad177@gmail.com>
2026-04-08 19:29:14 +0200

    Update Main.java

--- oopProject/src/Main.java
+++ oopProject/src/Main.java
@@ -1,9 +1,16 @@
 
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         RoomType t1 =new RoomType("single",1,200);
         System.out.println(  t1.getNO3());
         Amenity wifi = new Amenity("WiFi");
-        room.addAmenity(wifi);
+        Room r1=new Room();
+        r1.addAmenity(wifi);
+        System.out.println( r1.getAmenities().get(0));
+
+
     }
 }
