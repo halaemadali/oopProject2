1ba09d579608f065bd71dee8766c148966bfef49
halaemadali <halaemad177@gmail.com>
2026-04-08 17:57:44 +0200

    Update Main.java

--- oopProject/src/Main.java
+++ oopProject/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         RoomType t1 =new RoomType("single",1,200);
         System.out.println(  t1.getNO3());
+        Amenity wifi = new Amenity("WiFi");
+        room.addAmenity(wifi);
     }
 }
