e4bd7aeb46f3da0288409251f70b564b0b367cdc
Hagarhossamel-deen <hagarhossam100@gmail.com>
2026-04-08 17:02:24 +0200

    Update Room.java

--- oopProject/src/Room.java
+++ oopProject/src/Room.java
@@ -1,3 +1,21 @@
 public class Room {
+private int roomnumber;
+private RoomType type;
+private ArrayList<Amenity> amenities;
+private boolean isavailable ;
+private double price;
+    Room(){
     
+    }
+  public boolean checkavailability (boolean isavailable)  
+    { if (this.isavailable==true)
+        return true;
+     else 
+        return false;
+        
+    }
+    public void addamenity()
+    {
+        
+    }
 }
