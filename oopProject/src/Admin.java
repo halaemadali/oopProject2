291c65006ac084b3ab1c9a08d45a270bca42c4dc
saraelnahass <sara2007elnahass@gmail.com>
2026-04-09 23:48:25 +0200

    Update Admin.java

--- oopProject/src/Admin.java
+++ oopProject/src/Admin.java
@@ -1,1 +1,9 @@
 
+import java.time.LocalDate;
+
+public class Admin extends Staff {
+
+     public Admin(String username, String password, LocalDate dateOfBirth, int workingHours){
+         super(username, password,dateOfBirth,Role.ADMIN, workingHours);
+
+     }
