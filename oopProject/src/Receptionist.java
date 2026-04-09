8fd6ea5c01253cae92302ffa070fae2026a47a32
saraelnahass <sara2007elnahass@gmail.com>
2026-04-10 00:02:08 +0200

    Update Receptionist.java

--- oopProject/src/Receptionist.java
+++ oopProject/src/Receptionist.java
@@ -1,1 +1,20 @@
 
+import java.time.LocalDate;
+
+public class Receptionist extends Staff {
+  
+    public Receptionist(String username, String password , LocalDate dateOfBirth , int workingHours){
+        super(username, password, dateOfBirth,Role.RECEPTIONIST,workingHours);
+    }
+
+
+    public void checkIn(){
+      
+    }
+
+  
+    public void checkOut(){
+
+    }
+
+}
