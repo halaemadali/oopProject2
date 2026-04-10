60e049e1121b0c0d3d19eb20db8317fc30e7df34
saraelnahass <sara2007elnahass@gmail.com>
2026-04-10 16:27:31 +0200

    Update Admin.java

--- oopProject/src/Admin.java
+++ oopProject/src/Admin.java
@@ -10,16 +10,69 @@ public class Admin extends Staff {
 
      
        //add room
-     public void addRoom(Room room){
+     public void addRoom(int roomNumber, double price, boolean isAvailable, String NO3, int capacity, double basePrice) {
 
-     }
+        RoomType type = new RoomType(NO3, capacity, basePrice);
+
+        Room newRoom = new Room();
+        newRoom.setroomnumber(roomNumber);
+        newRoom.setPrice(price);
+        newRoom.setisavailable(isAvailable);
+        newRoom.settype(type);
+
+        HotelDatabase.rooms.add(newRoom);
+
+        System.out.println("Room " + roomNumber + " added successfully!");
+    }
+
+     
+       //view room
+    public void viewRoom(int roomNumber) {
+        for (Room r : HotelDatabase.rooms) {
+            if (r.getroomnumber() == roomNumber) {
+
+                System.out.println("\n=============VIEW ROOM ==============" );
+
+                System.out.println("Room Number: " + r.getroomnumber());
+                System.out.println("Price: " + r.getPrice());
+                System.out.println("Available: " + r.getisavailable());
+
+                // RoomType details
+                System.out.println("NO3: " + r.gettype().getNO3());
+                System.out.println("Capacity: " + r.gettype().getCapacity());
+                System.out.println("Base Price: " + r.gettype().getBasePrice());
+                System.out.println("Amenities: " + r.getAmenities());
+
+                System.out.println("======================================= \n");
+              return;
+            }
+        }
+        System.out.println("Room not found");
+    }
 
   
 
      //update room
-     public void updateRoom(){
-          
-     }
+    public void updateRoom(int roomnumber, double newPrice, boolean isAvailable, String NO3 , int capacity , double basePrice ) {
+       for (Room r : HotelDatabase.rooms) {
+           if (r.getroomnumber() == roomnumber) {
+
+               r.setPrice(newPrice);
+               r.setisavailable(isAvailable);
+
+               r.gettype().setNO3(NO3);
+               r.gettype().setCapacity(capacity);
+               r.gettype().setBasePrice(basePrice);
+
+
+               System.out.println("Room updated successfully");
+            return;
+           }
+       }
+       System.out.println("Room not found");
+
+   }
+
 
      
       //delete room
