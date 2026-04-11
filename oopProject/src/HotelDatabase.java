3695e93e1f204f75a640583dd78f1b94aa2af8d6
Mariam <101900968+Mariam12Mohamed@users.noreply.github.com>
2026-04-11 13:19:06 +0200

    Change guest, room, and reservation lists to instance variables

--- oopProject/src/HotelDatabase.java
+++ oopProject/src/HotelDatabase.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 class HotelDatabase {
 
     
-  public static ArrayList<Guest> guests = new ArrayList<>();
-    public static ArrayList<Room> rooms = new ArrayList<>();
-      public static ArrayList<Reservation> reservations = new ArrayList<>();
+  public  ArrayList<Guest> guests = new ArrayList<>();
+    public  ArrayList<Room> rooms = new ArrayList<>();
+      public  ArrayList<Reservation> reservations = new ArrayList<>();
     public static ArrayList< RoomType > roomTypes = new ArrayList<>();
     public static ArrayList<Amenity> amenities = new ArrayList<>();
 
