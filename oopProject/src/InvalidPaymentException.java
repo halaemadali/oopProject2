76ecaf8430eccbebd012fda1d9ab8f995a2069ac
EngSalmaShafek <salmaengcollege@gmail.com>
2026-04-16 14:53:49 +0200

    Add files via upload

---
+++ oopProject/src/InvalidPaymentException.java
@@ -0,0 +1,8 @@
+public class InvalidPaymentException extends Exception {
+    public InvalidPaymentException(){
+
+    }
+    public InvalidPaymentException(String message){
+        super(message);
+    }
+}
