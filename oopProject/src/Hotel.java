8e73e8638b8273fd7364f62d0554f7043ee9e6d9
halaemadali <halaemad177@gmail.com>
2026-04-19 00:58:33 +0200

    Create Hotel.java

---
+++ oopProject/src/Hotel.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Hotel {
+    private ArrayList<HotelAmenity> hotelAmenities = new ArrayList<>();
+
+    public void addAmenity(HotelAmenity a) {
+        hotelAmenities.add(a);
+    }
+}
