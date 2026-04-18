50244f91ec4c5d60eda5da0acc366407a6f29d4a
halaemadali <halaemad177@gmail.com>
2026-04-19 00:56:14 +0200

    Update RoomType.java

--- oopProject/src/RoomType.java
+++ oopProject/src/RoomType.java
@@ -1,54 +1,87 @@
 
 public class RoomType {
-    private RoomCategory roomcategory;    //single,double,suite
+    //single,double,suite
+    private String category;
     private int capacity;
+    private double BasePrice;
     private int roomTypeId;
+    private static int numofroomtypes =0;
 
-    public RoomType (RoomCategory roomcategory ,int capacity,int roomTypeId ){
-        this. roomcategory= roomcategory;
-        this.capacity=capacity;
 
-        this.roomTypeId=roomTypeId;
-    }
     public RoomType(){
-        this. roomcategory= RoomCategory.SINGLE;
-        this.capacity=1;
+        numofroomtypes ++;
+        this.roomTypeId = numofroomtypes;
+    }
+
+    public RoomType(String type, int capacity, double price){
+        numofroomtypes ++;
+        this.roomTypeId = numofroomtypes;
+        setCategory(type);
+        setCapacity(capacity);
+        setBasePrice(price);
 
     }
 
+    public String getCategory(){return category;}
 
-    public RoomCategory getRoomCategory(){
-        return  roomcategory;
+     public void setCategory(String type) {
+    if (type == null || type.trim().isEmpty()) {
+        throw new IllegalArgumentException("Room type cannot be empty");
     }
-    public void setRoomCategory(RoomCategory roomcategory){
-        if (roomcategory == null) {
-            throw new IllegalArgumentException("Room category cannot be null");
-        }
-        this.roomcategory = roomcategory;
+    if (!type.matches("[a-zA-Z ]+")) {
+        throw new IllegalArgumentException("Category must contain letters only");
     }
-    public int getCapacity(){
-        return capacity ;
+
+    this.category = type;
+
+    if (type.equalsIgnoreCase("Single")) {
+        this.capacity = 1;
+        System.out.println("Category is Single, Capacity is automatically set to 1");
+    } else if (type.equalsIgnoreCase("Double")) {
+        this.capacity = 2;
+        System.out.println("Category is Double, Capacity is automatically set to 2");
+    } else if (type.equalsIgnoreCase("Triple")) {
+        this.capacity = 3;
+        System.out.println("Category is Triple, Capacity is automatically set to 3");
     }
-    public void  setCapacity(int capacity){
+}
+
+    public void setCapacity(int capacity) {
         if (capacity <= 0) {
-            throw new IllegalArgumentException("Capacity must be greater than 0");
+            throw new IllegalArgumentException("Capacity must be positive");
         }
-        if (capacity > 10) {
-            throw new IllegalArgumentException("Capacity too large");
-        }
-
-        if (capacity >0)
-        {if (roomcategory == RoomCategory.SINGLE && capacity > 2) {
-            throw new IllegalArgumentException("Single room max capacity is 2");
+        if (capacity >=10) {
+            throw new IllegalArgumentException("Capacity cannot be more than 9");
         }
 
-            if (roomcategory == RoomCategory.DOUBLE && capacity > 4) {
-                throw new IllegalArgumentException("Double room max capacity is 4");
-            }
-            if (roomcategory == RoomCategory.SUITE && capacity >15) {
-                throw new IllegalArgumentException("suite max capacity is 15");
-            }
-            this.capacity = capacity;}
+        if (category != null &&
+                (category.equalsIgnoreCase("Single") ||
+                        category.equalsIgnoreCase("Double") ||
+                        category.equalsIgnoreCase("Triple"))) {
+
+            throw new IllegalArgumentException("Capacity is fixed for this category");
+        }
+
+        this.capacity = capacity;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public  double getBasePrice(){
+        return  BasePrice;
+    }
+    public void setBasePrice(double basePrice){
+        if (basePrice <= 0) {
+            throw new IllegalArgumentException("Base price must be >0");
+        }
+        if (basePrice > 10000000) { //  upper limit
+            throw new IllegalArgumentException("Price very high");
+        }
+        this.BasePrice = basePrice;
+        System.out.println(" price of  " +getCategory()+ " rooms is updated with new price :"+getBasePrice());
+
+
     }
 
     public void setroomTypeId(int roomTypeId){
@@ -57,4 +90,5 @@ public class RoomType {
     public int getroomTypeId(){
         return roomTypeId;
     }
+
 }
