1d85ed317680fd1f63afe2e2a3464e14bcd8cb7b
halaemadali <halaemad177@gmail.com>
2026-04-21 23:47:27 +0200

    Update Amenity.java

--- oopProject/src/Amenity.java
+++ oopProject/src/Amenity.java
@@ -1,12 +1,13 @@
+
+
 public class Amenity {
+
     private String name;
-    private double price;   // price per unit
+    private double price;
 
     public Amenity(String name, double price) {
         this.name = name;
         this.price = price;
-        HotelDatabase.amenities.add(this);
-
     }
 
     public String getName() {
@@ -17,29 +18,12 @@ public class Amenity {
         return price;
     }
 
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setPrice(double price) {
-        if (price < 0) {
-            throw new IllegalArgumentException("Price cannot be negative");
-        }
         this.price = price;
     }
 
-
     @Override
-    public String toString(){
-        return getName();
+    public String toString() {
+        return name + " - " + price;
     }
-
-    @Override
-    public String toString(){
-        return    "( " + name +
-                " price: "  + price + " )";
-
-    }
-
 }
