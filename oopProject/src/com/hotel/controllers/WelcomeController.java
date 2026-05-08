e79b4790cd65e2c46f4713522046e02f0f95d1d3
Sara <sara2007elnahass@gmail.com>
2026-05-08 10:38:39 +0300

    Welcome Screen

---
+++ oopProject/src/com/hotel/controllers/WelcomeController.java
@@ -0,0 +1,34 @@
+package com.hotel.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+
+public class WelcomeController {
+
+    @FXML private Button btnGuestLogin;
+    @FXML private Button btnStaffLogin;
+
+
+
+    @FXML
+    public void handleGuestLogin() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Resources/fxml/LoginScreen.fxml"));
+        Stage stage = (Stage) btnGuestLogin.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
+
+    @FXML
+    public void handleStaffLogin() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Resources/fxml/ReceptionistLoginScreen.fxml"));
+        Stage stage = (Stage) btnStaffLogin.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
+}
+
+
